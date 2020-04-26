9ad1797f9987dc7a5950cfeeb2265cfcfeb50670
teebu <teebu@users.noreply.github.com>
2020-04-26 00:54:54 +0000

    deploy: a69779707476ca0359a8cd43ba9a66aa73ef98f8

---
+++ precache-manifest.480013e3d48d81a3420a5e7068c9aa37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12eb6179ccc236b0ea3d45ab0822e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd5ae2c5a1892515b25",
+    "url": "/static/css/main.7276e988.chunk.css"
+  },
+  {
+    "revision": "eb91159a579ec5b92883",
+    "url": "/static/js/2.e58cf8bf.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.e58cf8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd5ae2c5a1892515b25",
+    "url": "/static/js/main.82c44297.chunk.js"
+  },
+  {
+    "revision": "92b1489e3e2e8d76a87a",
+    "url": "/static/js/runtime-main.9828b823.js"
+  }
+]);

ed34c1496809ee09cf1ca254cc8826385f3587ae
teebu <teebu@users.noreply.github.com>
2020-04-26 01:24:20 +0000

    deploy: 2adaa01d5034b5fc4f0574cfe52d3828a5ed03ba

---
+++ precache-manifest.ae379d35585a2b55381acab37bbf595a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39306924f31cb330d61cde62ebc4d21",
+    "url": "/react-cats/index.html"
+  },
+  {
+    "revision": "68d72f92f92d20106c87",
+    "url": "/react-cats/static/css/main.7276e988.chunk.css"
+  },
+  {
+    "revision": "eb91159a579ec5b92883",
+    "url": "/react-cats/static/js/2.e58cf8bf.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-cats/static/js/2.e58cf8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d72f92f92d20106c87",
+    "url": "/react-cats/static/js/main.9604df64.chunk.js"
+  },
+  {
+    "revision": "0ceaa5189e5712d16676",
+    "url": "/react-cats/static/js/runtime-main.968b7f61.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2d9bdda0c4a8cd204e717400365fc8f.js"
+  "/react-cats/precache-manifest.ae379d35585a2b55381acab37bbf595a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

db722e499d08c3b47ca54c151714864aecf0bf19
Y <junkbox0@gmail.com>
2020-04-29 16:47:37 -0700

    404 cat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import NotFoundImg from './images/404-cat.jpg'
 import Nav from "./helpers/nav";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,17 +16,26 @@ function App() {
       <div className="App">
         <Nav/>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" exact component={About}/>
           <Route path="/cats" exact><Cats size={15}/></Route>
-          <Route path="/breeds" component={Breeds} />
-          <Route path="/facts" component={Facts} />
-          <Route path="/cat/:id" exact component={CatDetail} />
+          <Route path="/breeds" component={Breeds}/>
+          <Route path="/facts" component={Facts}/>
+          <Route path="/cat/:id" exact component={CatDetail}/>
+          <Route component={NotFound}/>
         </Switch>
       </div>
     </Router>
   );
 }
 
+const NotFound = () => (
+  <div>
+    <Link to='/'>
+      <img src={NotFoundImg} alt="404 car" style={{display: 'block', margin: '40px auto', width: '40%'}}/>
+      You're lost. Go home!
+    </Link>
+  </div>
+)
 
 export default App;

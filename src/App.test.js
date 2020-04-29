459afe3b4b6ba77dd12f9c581048224a58e3a13c
Y <junkbox0@gmail.com>
2020-04-28 20:37:24 -0700

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,14 @@ import { render } from '@testing-library/react';
 import App from './App';
 import './__mocks__/intersectionObserverMock' //# observer errors if not mocking this "observer.disconnect is not a function"
 
-test('renders learn react link', () => {
+test('renders Meow!', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Meowz!/i);
-  expect(linkElement).toBeInTheDocument();
+  const ele = getByText(/Meow!/i);
+  expect(ele).toBeInTheDocument();
+});
+
+test('renders React Cats', () => {
+  const { getByText } = render(<App />);
+  const ele = getByText(/React Cats/i);
+  expect(ele).toBeInTheDocument();
 });

df17724e442ff6c81a81174c5a07fcd1bb13c040
Y <junkbox0@gmail.com>
2020-04-28 20:23:36 -0700

    added mocks and fixed testing

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,1 +1,6 @@
-export const removeEmpty = (obj) => { Object.keys(obj).forEach(key => obj[key] == null && delete obj[key]); };
+export const removeEmpty = obj => {
+  Object.keys(obj).forEach(key => {
+    if (obj[key] && typeof obj[key] === "object") removeEmpty(obj[key]); // recurse
+    else if (obj[key] == null) delete obj[key]; // delete
+  });
+};

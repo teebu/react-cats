0a66bf7f3479dc322e3d37f3b5a056453c22e72d
teebu <teebu@users.noreply.github.com>
2020-05-06 22:21:40 +0000

    deploy: 770b1b1e248d230881a91430fbb0e0828294d8f6

---
+++ precache-manifest.c2c8dedf908e29c1c6e4a7134b8cdfc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5ec7427daf0880b421fae271c264ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b99b2907ee9e32f85fe2",
+    "url": "./static/css/main.7276e988.chunk.css"
+  },
+  {
+    "revision": "a6aa60212975f329ad82",
+    "url": "./static/js/2.2462ee30.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.2462ee30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99b2907ee9e32f85fe2",
+    "url": "./static/js/main.3a469015.chunk.js"
+  },
+  {
+    "revision": "a9a787eb7b77a9cbbe9e",
+    "url": "./static/js/runtime-main.865f0d5f.js"
+  },
+  {
+    "revision": "9183cb908632a53bf619bbbcdfb68ae7",
+    "url": "./static/media/404-cat.9183cb90.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-cats/precache-manifest.b3a13ac4e21bdc2da9624b8db35a86d8.js"
+  "./precache-manifest.c2c8dedf908e29c1c6e4a7134b8cdfc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cats/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
